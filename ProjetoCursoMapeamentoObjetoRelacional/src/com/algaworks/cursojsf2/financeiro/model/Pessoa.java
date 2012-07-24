7065d0b99a3803f20aa90ea4048521be9a3a1efb
Thiago Faria de Andrade <thiagofa@gmail.com>
2012-07-23 22:25:38 -0300

    Projeto da aula sobre mapeamento objeto-relacional

---
+++ ProjetoCursoMapeamentoObjetoRelacional/src/com/algaworks/cursojsf2/financeiro/model/Pessoa.java
@@ -0,0 +1,65 @@
+package com.algaworks.cursojsf2.financeiro.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Pessoa implements Serializable {
+
+	private Integer codigo;
+	private String nome;
+	
+	public Pessoa() {
+	}
+	
+	public Pessoa(Integer codigo, String nome) {
+		this.codigo = codigo;
+		this.nome = nome;
+	}
+	
+	@Id
+	@GeneratedValue
+	public Integer getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(Integer codigo) {
+		this.codigo = codigo;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Pessoa other = (Pessoa) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
+}
